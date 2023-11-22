ba30f52d36deaea1b814d3bc61d292663577e877
Yatik Srivastava <yatiksrivastava1@gmail.com>
2023-11-22 22:16:10 +0530

    ::form

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,11 +2,13 @@ import React from 'react'
 import { Routes,Route } from 'react-router-dom'
 import Home from './pages/home/Home'
 import Survey from './pages/survey/Survey'
+import Form from './pages/form/Form'
 
 const App = () => {
   return ( 
     <Routes>
        <Route path='/' element={<Home />} />
+       <Route path='/form' element={<Form />} />
        <Route path='/questions' element={<Survey/>} />
     </Routes>
   )
