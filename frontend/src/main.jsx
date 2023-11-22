659361265aca1c47eda505b2217f19051e5e6734
Yatik Srivastava <yatiksrivastava1@gmail.com>
2023-11-22 22:16:51 +0530

    main

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,12 +3,10 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 import { BrowserRouter } from 'react-router-dom'
-import Navbar from './components/navbar/Navbar.jsx'
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <BrowserRouter>
-     <Navbar />
      <App />
   </BrowserRouter>
 )
