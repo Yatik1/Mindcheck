0a9650057c4730604472351c6d644b2e635e9c6d
Yatik Srivastava <yatiksrivastava1@gmail.com>
2023-12-06 14:37:12 +0530

    Thanks page

---
+++ frontend/src/pages/thanks/ThanksPage.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import "./style.scss"
+
+const ThanksPage = () => {
+    return  (
+        <div className='message'>
+          <span>Thank you for taking the time to complete this survey and contribute to our research. <br /> Your responses will be kept private. 🙌🎉</span>
+        </div>
+      ) 
+}
+
+export default ThanksPage
