45dd7c803affee4bccabaf767023b0302cf310bc
Yatik Srivastava <yatiksrivastava1@gmail.com>
2023-12-20 20:02:50 +0530

    dataSchema

--- backend/model/dataSchema.js
+++ backend/model/dataSchema.js
@@ -2,19 +2,19 @@ const mongoose = require('mongoose')
 
 const surveySchema = new mongoose.Schema(
     {
-        questionNumber : {
-         type: Number ,
-         required : true ,
-        } ,  
-       response : {
-         type:Number ,
-         required : true,
-        }                
-    } , 
+        questionNumber: {
+            type: Number,
+            required: true,
+        },
+        response: [{
+            type: Number,
+            required: true,
+        }],
+    },
     {
         timestamps: true,
     }
-) 
+);
 
 const Survey = mongoose.model("Survey Schema" , surveySchema);
 module.exports = Survey;
