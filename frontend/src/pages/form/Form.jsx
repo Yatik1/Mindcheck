e44c7057873e859f92b80881366a6b94763a218d
Yatik Srivastava <yatiksrivastava1@gmail.com>
2024-03-08 13:26:57 +0530

    form

--- frontend/src/pages/form/Form.jsx
+++ frontend/src/pages/form/Form.jsx
@@ -1,31 +1,55 @@
-import React from "react"
+import React, { useState } from "react"
 import { useNavigate } from 'react-router-dom'
-
+import axios from 'axios'
 import "./style.css"
 
 function Form() {
 
-    const navigate = useNavigate();
+  const [fullName , setFullName] = useState("") 
+  const [semester , setSemester] = useState("") 
 
-  const handleSurvey = () => {
-        navigate('/questions');
+  const navigate = useNavigate();
+
+  const handleSurvey = async (e) => {
+      e.preventDefault()
+      try {
+        await axios
+          .post("https://mindcheck-server.vercel.app/form" , {
+            fullName,
+            semester
+          }) 
+
+          console.log({
+            fullName,semester
+          });
+
+          console.log("Register successfully !");
+          navigate('/questions');
+
+      } catch (error) {
+        console.log("Signup error : " , error)
+      }
   }
   return (
-    <div className="page">
-    <div className="cover">
-      <h1>MINDCHECK</h1>
-      <input type="text" placeholder="Full Name" />
-      <input type="text" placeholder="Email Address" />
-      <input type="date" className="dob" placeholder="Date of Bith" />
-      <input type="text" placeholder="Year" />
-      <input type="text" placeholder="Stream" />
 
+    <form className="page" >
+      <div className="cover">
+       <h1>MINDCHECK</h1>
+       <input type="text" 
+              placeholder="Enter your Full Name"
+              value={fullName}
+              onChange={(e) => setFullName(e.target.value)}
+              required />
 
+       <input type="text" 
+              placeholder="Emter your Semester" 
+              value={semester}
+              onChange={(e) => setSemester(e.target.value)}
+              required />
 
-
-      <div className="btn" onClick={handleSurvey}>Let's Get Started</div>
-    </div>
-    </div>
+       <div className="btn" onClick={handleSurvey}>Let's Get Started</div>
+     </div>
+    </form>
   )
 }
 
