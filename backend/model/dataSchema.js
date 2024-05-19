dd89b81ec57a588c0ab70c7e2f07afcae35daff9
Yatik Srivastava <yatiksrivastava1@gmail.com>
2024-05-19 23:58:12 +0530

    debug

--- backend/model/dataSchema.js
+++ backend/model/dataSchema.js
@@ -1,24 +1,51 @@
 const mongoose = require('mongoose')
 
-const surveySchema = new mongoose.Schema(
-    {
-        questionNumber : {
-         type: Number ,
-         required : true ,
-        } ,  
-       response : {
-         type:Number ,
-         required : true,
-        } , 
-        user: {
-            type:mongoose.Schema.Types.ObjectId,
-            ref:"User",
-        }               
-    } , 
-    {
-        timestamps: true,
-    }
-) 
+// const surveySchema = new mongoose.Schema(
+//     {
+//         questionNumber : {
+//          type: Number ,
+//          required : true ,
+//         } ,  
+//        response : {
+//          type:Number ,
+//          required : true,
+//         } , 
+//         user: {
+//             type:mongoose.Schema.Types.ObjectId,
+//             ref:"User",
+//         }               
+//     } , 
+//     {
+//         timestamps: true,
+//     }
+// ) 
 
-const Survey = mongoose.model("Survey Schema" , surveySchema);
+// const Survey = mongoose.model("Survey Schema" , surveySchema);
+// module.exports = Survey;
+
+
+
+const mongoose = require('mongoose');
+
+const responseSchema = new mongoose.Schema({
+  questionNumber: {
+    type: Number,
+    required: true
+  },
+  response: {
+    type: String,
+    required: true
+  }
+});
+
+const surveySchema = new mongoose.Schema({
+  responses: [responseSchema],
+  createdAt: {
+    type: Date,
+    default: Date.now
+  }
+});
+
+const Survey = mongoose.model('Survey', surveySchema);
+
 module.exports = Survey;
