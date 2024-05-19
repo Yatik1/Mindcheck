78280897671b35e5139d0da29305894642a49935
Yatik Srivastava <yatiksrivastava1@gmail.com>
2024-05-20 01:30:57 +0530

    route

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,6 +3,7 @@ import { Routes,Route } from 'react-router-dom'
 import Home from './pages/home/Home'
 import Survey from './pages/survey/Survey'
 import Form from './pages/form/Form'
+import ThanksPage from './pages/thanks/ThanksPage'
 
 const App = () => {
   return ( 
@@ -10,6 +11,7 @@ const App = () => {
        <Route path='/' element={<Home />} />
        <Route path='/form' element={<Form />} />
        <Route path='/questions' element={<Survey/>} />
+       <Route path='/complete' element={<ThanksPage/>} />
     </Routes>
   )
 }
